a29ec0e232bfeb246ba657eeaa93cb55ae2d3e49
CodesDaniel <mailidanielifechukwu@gmail.com>
2020-01-15 21:24:05 +0100

    file cleanup

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom'
 import { BrowserRouter  , Route, Redirect, Switch } from 'react-router-dom';
 import  RouteWithLayout from "./components/RouteWIthLayout";
 import AuthScreen from './screens/AuthScreen';
