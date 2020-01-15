492f8212ddf16708f387985e1150f867271302ed
CodesDaniel <mailidanielifechukwu@gmail.com>
2020-01-15 15:47:59 +0100

    Add Table to dashboard

--- src/screens/UsersScreen.js
+++ src/screens/UsersScreen.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
- 
+ import {connect} from 'react-redux'
+ import * as action from '../actions'
+ import  {Table} from '../components/Users'
 class UsersScreen extends Component {
+    componentDidMount(){
+        this.props.dispatch(action.fetchAllUsers())
+        
+    }
     render() { 
         return (
             <div>
-                UsersScreen
+                 <Table data={this.props.users.allUsers}/>
             </div>
         );
     }
 }
+
+function mapStateToProps(state) {
+    return {
+      users: state.users
+    }
+  }
  
-export default UsersScreen;
+export default connect(mapStateToProps)(UsersScreen);
