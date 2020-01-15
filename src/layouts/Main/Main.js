d81b0f9db4e7ac1f058b7a738fb5d035ce72f43d
CodesDaniel <mailidanielifechukwu@gmail.com>
2020-01-15 21:23:48 +0100

    removed footer

--- src/layouts/Main/Main.js
+++ src/layouts/Main/Main.js
@@ -4,7 +4,7 @@ import clsx from 'clsx';
 import { makeStyles, useTheme } from '@material-ui/styles';
 import { useMediaQuery } from '@material-ui/core';
 
-import { Sidebar, Topbar, Footer } from './components';
+import { Sidebar, Topbar} from './components';
 
 const useStyles = makeStyles(theme => ({
   root: {
