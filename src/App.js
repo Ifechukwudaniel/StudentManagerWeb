eb4f6a1fc513589e9f55e0892e4dfa949ca66bd3
CodesDaniel <mailidanielifechukwu@gmail.com>
2020-01-15 17:17:31 +0100

    Changed app component from functonal to class component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React from 'react';
-import { PersistGate } from 'redux-persist/integration/react'
+import React, {Component} from 'react';
 import {ThemeProvider} from '@material-ui/styles'
 import theme from './theme'
 import { Provider } from 'react-redux';
 import  Navigation from './routes';
+
+
 const {store,persistor } = require('./reducers').init();
-
-function App() {
-  return (
-    <div className='App'>
-        <ThemeProvider theme= {theme}>
-         <Provider store={store}>
-             <Navigation/>
-         </Provider>
-         </ThemeProvider>
-    </div>
-  );
+const {tokenName, apiUrl} = require('./config')
+class App extends Component {
+  render() { 
+      return (
+        <div className='App'>
+            <ThemeProvider theme= {theme}>
+             <Provider store={store}>
+                 <Navigation/>
+             </Provider>
+             </ThemeProvider>
+        </div>
+      );
+  }
 }
-
-export default App;
+export default App
