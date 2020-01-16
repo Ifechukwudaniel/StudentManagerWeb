6c14b041016a622cb3b382cb5a3edcf423fd915d
CodesDaniel <mailidanielifechukwu@gmail.com>
2020-01-16 23:30:06 +0100

    Started user reducer

--- src/reducers/users-reducer.js
+++ src/reducers/users-reducer.js
@@ -1,5 +1,5 @@
 import { 
-    FETCH_ALL_USERS, FETCH_ALL_USERS_SUCCESS, FETCH_ALL_USERS_FAILURE
+     FETCH_ALL_USERS_SUCCESS, FETCH_ALL_USERS_FAILURE, CREATE_USER_FAILURE
 } from '../actions/types';
 
 const INITIAL_STATE = {
@@ -12,6 +12,8 @@ export const userReducer = (state = INITIAL_STATE, action) => {
     case FETCH_ALL_USERS_SUCCESS:
       return Object.assign({}, state,  {error:'',allUsers: action.users});
     case FETCH_ALL_USERS_FAILURE:
+      return Object.assign({}, state,  {error:action.error,allUsers: []});
+    case CREATE_USER_FAILURE:
       return Object.assign({}, state,  {error:action.error,allUsers: []});
     default:
       return state;
