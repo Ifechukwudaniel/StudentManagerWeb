9d0cc86dcfc947b8121eed89270f51aced1b9c6e
CodesDaniel <mailidanielifechukwu@gmail.com>
2020-03-11 23:42:47 +0100

    Added the attendance screen

---
+++ src/screens/AttendanceScreen.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+ 
+class AttendanceScreen extends Component {
+    render() { 
+        return (
+            <div>
+                AttendanceScreen
+            </div>
+        );
+    }
+}
+ 
+export default AttendanceScreen;
