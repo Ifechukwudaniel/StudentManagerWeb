fb9cba4a66a07c507a8967e48d6cb763ec30aa00
GAMHnile <georgeamhuweh@gmail.com>
2020-10-16 20:12:33 +0100

    added errorboundary and spinner coponent, used error boundary and spinner on routes.js

---
+++ src/components/Spinner/Spinner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Spinner.styles.css';
+
+
+const Spinner = props =>{
+    return (
+        <div className="spinner-overlay">
+            <div className="spinner-container" />
+        </div>
+    )
+}
+
+export default Spinner;
